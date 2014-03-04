db62fb62ca0a891c65365482d79dfa8286f77bb5
Adam Sunderland <iterion@gmail.com>
2014-03-04 10:00:28 -0600

    use non blocking io

--- lib/damage/client.rb
+++ lib/damage/client.rb
@@ -67,6 +67,7 @@ module Damage
     def read_message(socket)
       data = ""
       while buff = full_read_partial(socket)
+        info "readbuff: #{buff}"
         data << buff
       end
       info "RcvdChunk: #{data.gsub("\01", ", ")}"
