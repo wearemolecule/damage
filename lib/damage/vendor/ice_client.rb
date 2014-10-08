506feb14be8f9f0a1d2b092351d26415271bfea4
feverfunk <jay.skelly@gmail.com>
2014-10-07 20:57:30 -0500

    Fixed bugs uncovered during ice conformance testing: 1. set deleted_at to nil for bust/adjust. 2. subscription timestamp for trade capture updates.

--- lib/damage/vendor/ice_client.rb
+++ lib/damage/vendor/ice_client.rb
@@ -20,6 +20,7 @@ module Damage
           'TradeDate' => last_trade_date,
           'TransactTime' => last_transact_time
         }
+        binding.pry
         message_str = Message.new(schema, "TradeCaptureReportRequest", default_headers, params, { strict: strict? }).full_message
         Damage.configuration.logger.info "Requesting trade capture with snapshot and subscription"
         send_message(socket, message_str)
@@ -103,23 +104,12 @@ module Damage
       def last_transaction_time
         # ICE only stores current + previous day's history
         yesterday = Time.now.utc.yesterday.beginning_of_day
-        last_message = FixMessage.max(:created_at) || yesterday
-        history_last_message = FixMessageHistory.max(:created_at) || yesterday
+        # Get the last TradeCaptureReportReceived (msg_type == 'AE')
+        last_message = FixMessage.desc(:created_at).where(msg_type: 'AE').limit(1).first.try(:created_at) || yesterday
+        history_last_message = FixMessageHistory.desc(:created_at).where(msg_type: 'AE').limit(1).first.try(:created_at) || yesterday
         last_update = [last_message, history_last_message, yesterday].max
         last_update
       end
-
-      # def _last_transaction_time
-      #   last_missing_range_seqs = persistence.missing_message_ranges.flatten
-      #   if last_missing_range_seqs
-      #     last_received_seq_num = last_missing_range_seqs.first - 1
-      #     last_received_msg = rcvd_messages.select do |m|
-      #       m.msg_seq
-      #     end
-      #   else
-      #     Time.now - 2.days
-      #   end
-      # end
     end
   end
 end
